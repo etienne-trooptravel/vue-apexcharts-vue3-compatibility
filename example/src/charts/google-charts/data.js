3ca61206f59b7c09f547972e583ebfee8cab4e68
Etienne Theron <etienne@trooptravel.com>
2020-10-15 13:01:45 +0200

    added external data file

---
+++ example/src/charts/google-charts/data.js
@@ -0,0 +1,53 @@
+const contractors = [
+  {
+    companyName: "Trooptravel",
+    companyLogo:
+      "https://res-4.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/xgxvcsotbsyxqqd0v3wk",
+    role: "Founder",
+    name: "Leonard",
+    booking: true,
+    start: new Date(new Date().setDate(new Date().getDate())),
+    end: new Date(new Date().setDate(new Date().getDate() + 3))
+  },
+  {
+    companyName: "Tesla",
+    companyLogo:
+      "https://www.logo.wine/a/logo/Tesla%2C_Inc./Tesla%2C_Inc.-Logo.wine.svg",
+    role: "Senior Developer",
+    name: "Ignatius",
+    booking: false,
+    start: new Date(new Date().setDate(new Date().getDate() + 1)),
+    end: new Date(new Date().setDate(new Date().getDate() + 3))
+  },
+  {
+    companyName: "Trooptravel",
+    companyLogo:
+      "https://res-4.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/xgxvcsotbsyxqqd0v3wk",
+    role: "Founder",
+    name: "Dennis",
+    booking: true,
+    start: new Date(new Date().setDate(new Date().getDate() + 1)),
+    end: new Date(new Date().setDate(new Date().getDate() + 3))
+  },
+  {
+    companyName: "Landobyte",
+    companyLogo:
+      "https://res-3.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/pd3irshwgvjuxqu9y0i9",
+    role: "Lead Developer",
+    name: "Rhyno",
+    booking: true,
+    start: new Date(new Date().setDate(new Date().getDate() + 4)),
+    end: new Date(new Date().setDate(new Date().getDate() + 6))
+  },
+  {
+    companyName: "Trooptravel",
+    companyLogo:
+      "https://res-4.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/xgxvcsotbsyxqqd0v3wk",
+    role: "Junior Developer",
+    name: "Etienne",
+    booking: false,
+    start: new Date(new Date().setDate(new Date().getDate() + 2)),
+    end: new Date(new Date().setDate(new Date().getDate() + 3))
+  }
+];
+export default contractors;
