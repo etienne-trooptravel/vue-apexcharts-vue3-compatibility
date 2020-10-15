88d406f4a3a939b7d88f3199439ac5f6de8ceec6
Etienne Theron <etienne@trooptravel.com>
2020-10-15 08:10:34 +0200

    added tooltip

--- example/src/charts/google-charts/Policy.vue
+++ example/src/charts/google-charts/Policy.vue
@@ -89,6 +89,8 @@ export default {
     const contractors = ref([
       {
         companyName: "Trooptravel",
+        companyLogo:
+          "https://res-4.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/xgxvcsotbsyxqqd0v3wk",
         role: "Founder",
         name: "Leonard",
         booking: true,
@@ -97,6 +99,8 @@ export default {
       },
       {
         companyName: "Tesla",
+        companyLogo:
+          "https://www.logo.wine/a/logo/Tesla%2C_Inc./Tesla%2C_Inc.-Logo.wine.svg",
         role: "Senior Developer",
         name: "Ignatius",
         booking: false,
@@ -105,6 +109,8 @@ export default {
       },
       {
         companyName: "Trooptravel",
+        companyLogo:
+          "https://res-4.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/xgxvcsotbsyxqqd0v3wk",
         role: "Founder",
         name: "Dennis",
         booking: true,
@@ -113,6 +119,8 @@ export default {
       },
       {
         companyName: "Landobyte",
+        companyLogo:
+          "https://res-3.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/pd3irshwgvjuxqu9y0i9",
         role: "Lead Developer",
         name: "Rhyno",
         booking: true,
@@ -121,6 +129,8 @@ export default {
       },
       {
         companyName: "Trooptravel",
+        companyLogo:
+          "https://res-4.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/xgxvcsotbsyxqqd0v3wk",
         role: "Junior Developer",
         name: "Etienne",
         booking: false,
@@ -220,7 +230,8 @@ export default {
           arr[i + 1] = [
             label,
             contractors.value[i].name,
-            `<div>Custom Tooltip: ${contractors.value[i].name}</div>`,
+            chartTooltipHTML(i),
+            // `<div>Custom Tooltip: ${contractors.value[i].name}</div>`,
             bookingStyle(i),
             contractors.value[i].start,
             contractors.value[i].end
@@ -276,6 +287,46 @@ export default {
       focusTarget: "category", // This line makes the entire category's tooltip active.
       tooltip: { isHtml: true } // Use an HTML tooltip.
     });
+
+    function chartTooltipHTML(i) {
+      return (
+        '<div id="container">' +
+        '<div id="blog-card">' +
+        '<a class="card-link" href="#">' +
+        '<article class="blog-card">' +
+        '<img class="tooltip-logo" :src="' +
+        contractors.value[i].companyLogo +
+        " />" +
+        '<div class="tooltip-details">' +
+        '<h4 class="tooltip-name">' +
+        contractors.value[i].name +
+        "</h4>" +
+        '<h3 class="tooltip-date">' +
+        contractors.value[i].start.toLocaleString("default", {
+          month: "short"
+        }) +
+        " " +
+        contractors.value[i].start.getDate() +
+        " - " +
+        contractors.value[i].end.getDate() +
+        ", " +
+        contractors.value[i].start.getFullYear() +
+        "</h3>" +
+        '<p class="tooltip-duration"> Duration: ' +
+        (contractors.value[i].end - contractors.value[i].start) /
+          (1000 * 3600 * 24) +
+        " days" +
+        "</p>" +
+        '<p class="tooltip-role">' +
+        contractors.value[i].role +
+        "</p>" +
+        "</div>" +
+        "</article>" +
+        "</a>" +
+        "</div>" +
+        "</div>"
+      );
+    }
 
     // function onChartReady(chart, google) {
     // //   console.log(`chart`, chart);
@@ -360,7 +411,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .select-sort-field {
   text-transform: capitalize;
 }
@@ -370,5 +421,143 @@ export default {
 }
 .chart {
   height: 400px;
+}
+
+$text: #777;
+$black: #121212;
+$white: #fff;
+$red: #e04f62;
+$border: #ebebeb;
+$shadow: rgba(0, 0, 0, 0.2);
+
+@mixin transition($args...) {
+  transition: $args;
+}
+
+* {
+  box-sizing: border-box;
+  &::before,
+  &::after {
+    box-sizing: border-box;
+  }
+}
+
+#container {
+  width: 18rem;
+  height: 10rem;
+}
+
+.blog-card {
+  display: flex;
+  flex-direction: row;
+  background: $white;
+  box-shadow: 0 0.1875rem 1.5rem $shadow;
+  border-radius: 0.375rem;
+  overflow: hidden;
+}
+
+.card-link {
+  position: relative;
+  display: block;
+  color: inherit;
+  text-decoration: none;
+  padding: 0;
+  border: none;
+  &:hover .tooltip-name {
+    @include transition(color 0.3s ease);
+    color: $red;
+  }
+  &:hover .tooltip-logo {
+    @include transition(opacity 0.3s ease);
+    opacity: 0.9;
+  }
+}
+
+.tooltip-logo {
+  @include transition(opacity 0.3s ease);
+  display: block;
+  width: 100%;
+  object-fit: cover;
+}
+
+.tooltip-details {
+  padding: 0.5rem;
+}
+
+.tooltip-name {
+  @include transition(color 0.3s ease);
+  display: inline-block;
+  text-transform: uppercase;
+  font-size: 0.75rem;
+  font-weight: 700;
+  line-height: 1;
+  letter-spacing: 0.0625rem;
+  margin: 0 0 0.5rem 0;
+  padding: 0 0 0.5rem 0;
+  border-bottom: 0.125rem solid $border;
+}
+
+.tooltip-duration {
+  margin: 0;
+}
+
+.tooltip-date {
+  @include transition(color 0.3s ease);
+  font-size: 1rem;
+  line-height: 1.4;
+  color: $black;
+  font-weight: 400;
+  margin: 0 0 0 0;
+}
+
+.tooltip-role {
+  font-size: 0.875rem;
+  line-height: 1;
+  margin: 0.5rem 0 0 0;
+  padding: 0.5rem 0 0 0;
+  border-top: 0.0625rem solid $border;
+}
+
+// @media (max-width: 40rem) {
+//   #container {
+//     width: 10rem;
+//     height: 12rem;
+//   }
+
+//   .blog-card {
+//     flex-wrap: wrap;
+//   }
+// }
+
+@supports (display: grid) {
+  body {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 0.625rem;
+    grid-template-areas: ". main main ." ". main main .";
+  }
+
+  #container {
+    grid-area: main;
+    align-self: center;
+    justify-self: center;
+  }
+
+  .tooltip-logo {
+    height: 100%;
+  }
+
+  .blog-card {
+    display: grid;
+    grid-template-columns: 2fr 3fr;
+    grid-template-rows: 1fr;
+  }
+
+  //   @media (max-width: 40rem) {
+  //     .blog-card {
+  //       grid-template-columns: auto;
+  //       grid-template-rows: 12rem 1fr;
+  //     }
+  //   }
 }
 </style>
